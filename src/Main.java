6366b74c55f7843e2104fd6d55cab34f9009759b
amanraj1105 <aman1105raj@gmail.com>
2025-12-19 11:32:41 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Bridgelabz");
+public class main {
+    public main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
