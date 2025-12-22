436c7705382f185e157be3a61e4dabd291c5536a
amanraj1105 <aman1105raj@gmail.com>
2025-12-22 16:28:59 +0530

    [Aman] : added : strings

---
+++ src/Strings(Package)/Level 1/CharArrayComparison.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CharArrayComparison {
+
+    public static char[] getCharacters(String text) {
+        char[] arr = new char[text.length()];
+        for (int i = 0; i < text.length(); i++) {
+            arr[i] = text.charAt(i);
+        }
+        return arr;
+    }
+
+    public static boolean compareArrays(char[] a, char[] b) {
+        if (a.length != b.length) return false;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+
+        char[] customArray = getCharacters(text);
+        char[] builtInArray = text.toCharArray();
+
+        System.out.println(compareArrays(customArray, builtInArray));
+    }
+}
