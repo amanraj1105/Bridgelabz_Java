a09cc81e8540240736437a2a98c9d1d8966f90a9
amanraj1105 <aman1105raj@gmail.com>
2026-01-10 12:15:47 +0530

    Removed previous branch files for fresh start

--- src/ProgrammingElements/Level1/KmToMiles.java
+++
@@ -1,9 +0,0 @@
-public class KmToMiles {
-    public static void main(String[] args) {
-        double kilometers = 10.8;
-        double conversionFactor = 1.6;
-        double miles = kilometers / conversionFactor;
-        
-        System.out.println(kilometers + " kilometers is equal to " + miles + " miles.");
-    }
-}
